949351ad80860c5e0d26eaf40b3616a5ff811f8c
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-08-22 13:47:32 -0500

    pep8

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -11,24 +11,23 @@ api.env.hosts = ['3.80.226.140', '54.83.103.89']
 
 def do_clean(number=0):
         """ Deletes olds versions """
+        n = int(number)
+        if n == 0:
+                n = 1
+        versions = sorted(os.listdir("versions"))
+        for files in range(number):
+                versions.pop()
+        with api.lcd("versions"):
+                for fl in versions:
+                        api.local("rm {}".format(fl))
 
-    n = int(number)
-    if n == 0:
-        n = 1
-    versions = sorted(os.listdir("versions"))
-    for files in range(number):
-        versions.pop()
-    with api.lcd("versions"):
-        for fl in versions:
-            api.local("rm {}".format(fl))
-
-    with cd("/data/web_static/releases/"):
-        vers_files = api.run("ls -tr").split()
+        with cd("/data/web_static/releases/"):
+                vers_files = api.run("ls -tr").split()
         fls = []
         for f in vers_files:
-            if "web_static" in f:
-                fls.append(f)
-            for f in range(n):
-                vers_files.pop()
-            for f in vers_files:
-                api.run("rm -rf ./{}".format(f))
+                if "web_static" in f:
+                        fls.append(f)
+                for f in range(n):
+                        vers_files.pop()
+                for f in vers_files:
+                        api.run("rm -rf ./{}".format(f))
